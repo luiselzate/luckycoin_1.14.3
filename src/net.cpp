77b82fd31ac996be4dc6a13d7336aa7a236e66f1
luiselzate <160407563+luiselzate@users.noreply.github.com>
2025-03-17 08:31:13 +0200

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1468,7 +1468,7 @@ void ThreadMapPort()
             }
         }
 
-        std::string strDesc = "Dogecoin " + FormatFullVersion();
+        std::string strDesc = "Luckycoin " + FormatFullVersion();
 
         try {
             while (true) {
