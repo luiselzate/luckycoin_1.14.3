1b4d367ca25c65f38ee65d3fa2e45432bb9ecddf
root <luiselzate@gmail.com>
2025-04-02 16:06:43 +0300

    unit symbol

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -44,11 +44,11 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case MBTC: return QString("MDOGE");
-    case kBTC: return QString("kDOGE");
-    case BTC: return QString("DOGE");
-    case mBTC: return QString("mDOGE");
-    case uBTC: return QString::fromUtf8("μDOGE");
+    case MBTC: return QString("MLKY");
+    case kBTC: return QString("kLKY");
+    case BTC: return QString("LKY");
+    case mBTC: return QString("mLKY");
+    case uBTC: return QString::fromUtf8("μLKY");
     default: return QString("???");
     }
 }
@@ -57,11 +57,11 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case MBTC: return QString("Mega-Dogecoins (1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case kBTC: return QString("Kilo-Dogecoins (1" THIN_SP_UTF8 "000)");
-    case BTC: return QString("Dogecoins");
-    case mBTC: return QString("Milli-Dogecoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Dogecoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case MBTC: return QString("Mega-Luckycoins (1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case kBTC: return QString("Kilo-Luckycoins (1" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Luckycoins");
+    case mBTC: return QString("Milli-Luckycoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Luckycoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
