8ebaa06b89c67f7f3563cd1b80e2537bb446723e
luiselzate <160407563+luiselzate@users.noreply.github.com>
2025-04-10 14:05:01 +0200

    Update splashscreen.cpp
    update splash

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -72,7 +72,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     pixPaint.fillRect(rGradient, gradient);
 
     // draw the bitcoin icon, expected size of PNG: 1024x1024
-    QRect rectIcon(QPoint(-150,-122), QSize(430,430));
+    QRect rectIcon(QPoint(10,10), QSize(200,200));
 
     const QSize requiredSize(1024,1024);
     QPixmap icon(networkStyle->getAppIcon().pixmap(requiredSize));
